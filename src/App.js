fa408a4df9fed7b8dfabf1e7bd67e72490d08935
Collins Murgor <vintoncollins702@gmail.com>
2023-07-25 08:40:17 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+function Item({ name, isPacked }) {
+  return <li className="item">{isPacked ? <del>{name + "✔"}</del> : name}</li>;
+}
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Packing List</h1>
+      <Item isPacked={true} name="Dog" />
+      <Item Ispacked={true} name="Camping Truck" />
+      <Item isPacked={false} name="Girlfriend" />
+    </section>
+  );
+}
